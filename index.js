739a15985b5efd218af737830c1adc4c3fdee94f
Souvik Basu <souvikbasu@gmail.com>
2021-06-10 23:55:48 -0700

    build(server): provide port from process env

--- index.js
+++ index.js
@@ -7,6 +7,6 @@ const server = new ApolloServer({
   resolvers: userResolvers,
 });
 
-server.listen().then(({ url }) => {
+server.listen({ port: process.env.PORT || 4000 }).then(({ url }) => {
   console.log(`🚀  Server ready at ${url}`);
 });
