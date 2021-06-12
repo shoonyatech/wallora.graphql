a2b0c38089b22bd9ee55f6d79c548d5e21523409
Souvik Basu <souvikbasu@gmail.com>
2021-06-12 14:26:21 +0530

    chore(refactor): merge resolver and schema for each typedef into single files

--- resolvers/charts.js
+++
@@ -1,9 +0,0 @@
-import predictedSavings from "../dummy-data/predicted-savings.js";
-
-const chartsResolvers = {
-  Query: {
-    charts: () => ({ predictedSavings }),
-  },
-};
-
-export default chartsResolvers;
