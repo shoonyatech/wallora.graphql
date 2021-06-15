8db611248cd5f414a4f70964fed8ede05cbbc2e6
Priyanka Jayaswal <priyankajayaswal025@gmail.com>
2021-06-16 00:32:57 +0530

    feat(charts): fetch data from wallora rest api for plannedExpenses

---
+++ dummy-data/planned-expenses.js
@@ -0,0 +1,48 @@
+const plannedExpenses = [
+    {
+      name: "House rent",
+      amount: 2400,
+    },
+    {
+      name: "Taxes",
+      amount: 1398,
+    },
+    {
+      name: "Grocery",
+      amount: 9800,
+    },
+    {
+      name: "Eat out",
+      amount: 3908,
+    },
+    {
+      name: "Bills and EMIs",
+      amount: 4800,
+    },
+    {
+      name: "Fitness",
+      amount: 3800,
+    },
+    {
+      name: "Entertainment",
+      amount: 4300,
+    },
+    {
+      name: "Entertainment",
+      amount: 0,
+    },
+    {
+      name: "Entertainment",
+      amount: 0,
+    },
+    {
+      name: "Entertainment",
+      amount: 0,
+    },
+    {
+      name: "Entertainment",
+      amount: 0,
+    },
+];
+
+export default plannedExpenses;
