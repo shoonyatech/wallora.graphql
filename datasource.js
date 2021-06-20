6e537299647adc63901d85dd09d8741e6762a36f
Souvik Basu <souvikbasu@gmail.com>
2021-06-20 01:56:10 -0700

    feat(datasource): send access token in authorization header of REST api. Change REST api URL

--- datasource.js
+++ datasource.js
@@ -3,14 +3,14 @@ import { RESTDataSource } from "apollo-datasource-rest";
 export class WalloraAPI extends RESTDataSource {
   constructor() {
     super();
-    this.baseURL = "https://www.wallora.com/api/";
+    this.baseURL = "https://wallora-api.herokuapp.com/api/";
   }
 
   async getUserSettings(accessToken) {
     return this.get("user-settings", null, {
       cacheOptions: { ttl: 60 },
       headers: {
-        "X-Access-Token": accessToken,
+        authorization: accessToken,
       },
     });
   }
