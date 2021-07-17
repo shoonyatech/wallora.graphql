b7ad37ca67fb181a7f6ec55841f3d6120cd3fb05
Priyanka Jayaswal <priyankajayaswal025@gmail.com>
2021-07-17 23:07:37 +0530

    feat(currencies): add graphql support for currencies api

--- datasource.js
+++ datasource.js
@@ -1,6 +1,14 @@
 import { RESTDataSource } from "apollo-datasource-rest";
 
 export class WalloraAPI extends RESTDataSource {
+
+  getHeaders(v1AccessToken, v2AccessToken) {
+    return  { 
+      "x-access-token": v1AccessToken,
+      authorization: `Bearer ${v2AccessToken}`,
+    }
+  }
+
   constructor() {
     super();
     this.baseURL = "https://wallora-api.herokuapp.com/api/";
@@ -9,19 +17,13 @@ export class WalloraAPI extends RESTDataSource {
   async getUserSettings(v1AccessToken, v2AccessToken) {
     return this.get("user-settings", null, {
       cacheOptions: { ttl: 60 },
-      headers: {
-        "x-access-token": v1AccessToken,
-        authorization: `Bearer ${v2AccessToken}`,
-      },
+      headers: this.getHeaders(v1AccessToken, v2AccessToken),
     });
   }  
   async getWorkItems(v1AccessToken,v2AccessToken) {
-    return this.get("workitems",null, { 
-      headers: {
-        "x-access-token": v1AccessToken,
-        authorization: `Bearer ${v2AccessToken}`,
-      }
-    })
+    return this.get("workitems",null, { headers: this.getHeaders(v1AccessToken, v2AccessToken) })
   }
-
+  async getCurrencies(v1AccessToken,v2AccessToken) {
+    return this.get("currencies",null, { headers: this.getHeaders(v1AccessToken, v2AccessToken) })
+  }
 }
