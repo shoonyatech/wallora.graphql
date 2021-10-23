b16ea1e6ade1911961cd61be990ff8ba24c2cfd1
rajakvk <rajakvk@gmail.com>
2021-10-23 08:42:55 +0530

    feat(actuals): Add schema, resolver as per new data structure

--- schema/finance.js
+++ schema/finance.js
@@ -1,21 +1,74 @@
 import { gql } from "apollo-server";
 
 const typeDef = gql`
-  
+
   type Finance {
-    workItems: [WorkItems]
+    currentMonth: String
+    incomeExpenseCategories: [incomeExpenseCategoriesWorkItems]
+    actuals: actualsWorkItems
+    plan: planMonths
   }
 
-  type WorkItems {
-    accountId: String
-    id: String
-    incomeOrExpense: String
+  type incomeExpenseCategoriesWorkItems {
+    id: Int
+    order: Int
     name: String
-    order: Int 
+    incomeOrExpense: String
+    currency: String
+    plannedTotal: Int
+    actualTotal: Int
   }
-  
+
+  type actualsWorkItems {
+    dates: [actualsDates]
+  }
+
+  type actualsDates {
+    date: String
+    totalSpent: Int
+    workItems: [actualsDatesWorkItems]
+  }
+
+  type actualsDatesWorkItems {
+    id: Int
+    incomeOrExpense: String
+    category: String
+    lineItems: [lineItemsWorkItems]
+  }
+
+  type lineItemsWorkItems {
+    description: String
+    currency: String
+    amount: Int
+    tags: [String]
+  }
+
+  type planMonths {
+    months: [planMonthsWorkItems]
+  }
+
+  type planMonthsWorkItems {
+    month: String
+    workItems: [planMonthsWorkItemsWorkItems]
+  }
+
+  type planMonthsWorkItemsWorkItems {
+    id: Int
+    incomeOrExpense: String
+    category: String
+    lineItems: [planMonthsWorkItemsWorkItemsLineItems]
+  }
+
+  type planMonthsWorkItemsWorkItemsLineItems {
+    description: String
+    currency: String
+    amount: Int
+    tags: [String]
+  }
+
   extend type Query {
     finance: Finance
+    actualsDates(startDate: String!, endDate: String!): [actualsDates]
   }
 `;
 
@@ -29,7 +82,23 @@ const financeResolvers = {
       )
       return {workItems };
     },
-  },
+    actualsDates: async (root, args, { v1AccessToken, v2AccessToken, dataSources }) => {
+      const workItems = await dataSources.walloraAPI.getActualsDatesWorkItems(
+        v1AccessToken,
+        v2AccessToken,
+        args
+        );
+      let dates = [];
+      workItems.map((item, index) => {
+        if (index > 0 && item.date === workItems[index - 1].date) {
+          dates[index - 1] = { date: item.date, totalSpent: workItems[index - 1].amount + item.amount };
+        } else {
+          dates.push({ date: item.date, totalSpent: item.amount });
+        }
+      });
+      return dates;
+    },
+  }
 };
 
 export { typeDef, financeResolvers };
