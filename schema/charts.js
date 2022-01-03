659c51bdb0f0270a3a8970769ef62d03de599f78
Syed Rasheduddin <SyedRashed007@users.noreply.github.com>
2022-01-03 11:57:12 +0530

    feat(dashboard): fetch data from wallora rest api for charts

--- schema/charts.js
+++ schema/charts.js
@@ -1,8 +1,6 @@
 import { gql } from "apollo-server";
 import predictedSavings from "../dummy-data/predicted-savings.js";
 import plannedExpenses from "../dummy-data/planned-expenses.js";
-import myCurrentWorth from '../dummy-data/my-current-worth.js';
-import myOweData from '../dummy-data/my-owe-data.js';
 
 const typeDef = gql`
   extend type Query {
@@ -10,20 +8,69 @@ const typeDef = gql`
   }
 
   type Charts {
-    myCurrentWorth: [TotalWorth]
+    banks: [TotalWorth]
+    investments: [TotalInvestments]
+    cashAtHome: TotalCashAtHome
+    creditCards: [TotalCreditCards]
+    loans: [TotalLoans]
+    realEstate: TotalRealEstate
+    fixedDeposits: TotalFixedDeposits
+    goldAndPreciousMetals: TotalGoldAndPreciousMetals
+    mutualFund: TotalMutualFund
+    stocks: TotalStocks
     predictedSavings: [SavingsThisMonth]
     plannedExpenses: [WorkItemWiseSavings]
-    myOweData: [TotalOweWorth]
   }
   
-  type TotalOweWorth {
-    name: String
+  type TotalStocks {
+    amount: Int
+  }
+
+  type TotalMutualFund {
     amount: Float
   }
 
-  type TotalWorth {
-    name: String
+  type TotalGoldAndPreciousMetals {
     amount: Float
+  }
+
+  type TotalFixedDeposits {
+    amount: Float
+  }
+
+  type TotalRealEstate {
+    amount: Float
+  }
+
+  type TotalLoans {
+    name: String
+    outstandingPrincipal: Float
+    currency: String
+  }
+
+  type TotalCreditCards {
+    availableCreditLimit: Float
+    currency: String
+    outstandingBalance: Float
+    creditLimit: Float
+  }
+
+  type TotalCashAtHome {
+    amount: Float
+    date: String
+    currency: String
+  }
+
+  type TotalInvestments {
+    currentValue: Float
+    currency: String
+    investmentName: String
+  }
+
+  type TotalWorth {
+    bankName: String
+    balance: Float
+    currency: String
   }
 
   type SavingsThisMonth {
@@ -44,8 +91,30 @@ const typeDef = gql`
 
 const chartsResolvers = {
   Query: {
-    charts: (root, {}, { v1AccessToken, v2AccessToken, dataSources }) => {
-      return { myCurrentWorth, myOweData, predictedSavings, plannedExpenses };
+    charts: async (root, {}, { v1AccessToken, v2AccessToken, dataSources }) => {
+      const banks = await dataSources.walloraAPI.getBanks(
+        v1AccessToken,
+        v2AccessToken
+      );
+      const investments = await dataSources.walloraAPI.getInvestments(
+        v1AccessToken,
+        v2AccessToken
+      );
+      const cashAtHome = await dataSources.walloraAPI.getCashAtHome(
+        v1AccessToken,
+        v2AccessToken
+      );
+      const creditCards = await dataSources.walloraAPI.getCreditCards(
+        v1AccessToken,
+        v2AccessToken
+      );
+      const loans = await dataSources.walloraAPI.getLoans(
+        v1AccessToken,
+        v2AccessToken
+      ); 
+      const {realEstate, fixedDeposits, goldAndPreciousMetals, mutualFund, stocks} = 0;
+
+      return { realEstate, fixedDeposits, goldAndPreciousMetals, mutualFund, stocks, banks, investments, cashAtHome, creditCards, loans, predictedSavings, plannedExpenses };
     },
     
   },
